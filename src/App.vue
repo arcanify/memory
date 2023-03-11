d6d2c98f01cda1786956fb3967d61edb7ffa1725
jakubserwin <kuba.serwin99@gmail.com>
2023-03-11 22:18:36 +0100

    add path aliases and i18n

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import i18n from '@/i18n'
+const { t } = i18n.global
+
+const msg = t('hello')
+</script>
 
 <template>
   <h1 class="text-3xl font-bold underline bg-red-300">
-    Hello world with TailWind!
+    {{ msg }}
   </h1>
 </template>
 
