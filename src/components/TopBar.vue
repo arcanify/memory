d30b2f96ebee8a2b93f43b683b8b74b955f13319
Wojciech Sutkowski <wojciechsutkowskii@gmail.com>
2023-03-29 22:03:24 +0200

    Top bar component created with props

---
+++ src/components/TopBar.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { GameUsers, Score } from '@/types'
+
+const props = defineProps<{
+  category: string,
+  users: GameUsers,
+  score: Score
+}>()
+</script>
+
+<template>
+  <div
+    class="container flex flex-col text-center gap-16 items-center mt-44 w-full"
+  >
+    <h1 class="text-3xl font-bold p-8 text-[var(--main)]">
+      {{ props }}
+    </h1>
+  </div>
+</template>
---
+++ src/types/game.ts
@@ -0,0 +1,11 @@
+import { User } from './user'
+
+export interface Score {
+  scoreUser1: number
+  scoreUser2: number
+}
+
+export interface GameUsers {
+  user1: User
+  user2: User
+}
